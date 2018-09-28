cd85f2d44bf16bc1f345975ae1a4c027919c23b2
Evan <yao_lei_java@hotmail.com>
2018-09-28 16:10:57 +0800

    获取图片列表

--- miniprogram/pages/test/test.js
+++ miniprogram/pages/test/test.js
@@ -5,7 +5,7 @@ Page({
   data: {
     newTest: 'New Test',
     queryResult: [],
-    imgPath:''
+    imgPath:[]
   },
 
   onLoad: function() {
@@ -20,25 +20,46 @@ Page({
     })
   },
 
-  getImgs: function() {
-
+  getTemImgs: function(fileIds) {
       // wx.showLoading({
       //     title: '上传中',
       // });
 
     // wx.cloud.downloadFile({
-    //   fileID: 'cloud://hiagenv-9dce7d.6869-hiagenv-9dce7d/my-image.png', // 文件 ID
-      
+    //   fileID: fileId, // 文件 ID
     //   success: res => {
-    //     // 返回临时文件路径
     //     debugger
-    //     console.log('@@@@@');
+    //     // this.setData({
+    //     //   filePath
+    //     // })
     //   },
     //   fail: console.error,
     //   complete: () => {
     //     wx.hideLoading()
     //   }
     // })
+
+    wx.cloud.getTempFileURL({
+      fileList: fileIds,
+      success: res => {
+        // res.fileList[0].tempFileURL
+
+        // fileList 是一个有如下结构的对象数组
+        // [{
+        //    fileID: 'cloud://xxx.png', // 文件 ID
+        //    tempFileURL: '', // 临时文件网络链接
+        //    maxAge: 120 * 60 * 1000, // 有效期
+        // }]
+        // console.log(res.fileList)
+        this.setData({
+          imgPath:res.fileList
+        })
+        console.log('图片临时地址', res)
+      },
+      fail: console.error
+    })
+
+
   },
 
 
@@ -72,6 +93,7 @@ Page({
 
   onQuery: function () {
     const db = wx.cloud.database()
+    const self = this;
     // 查询当前用户所有的 counters
     db.collection('TestCounters').where({
       _openid: this.data.openid
@@ -80,8 +102,13 @@ Page({
         this.setData({
           queryResult: res.data
           // queryResult: JSON.stringify(res.data, null, 2)
-          // cloud://hiagenv-9dce7d.6869-hiagenv-9dce7d/my-image.png
         })
+        let imgFileIds = []; //图片fileId集合
+        for (let i = 0; i< res.data.length; i++) {
+          imgFileIds.push(res.data[i].fileID);
+        }
+        //获取图片临时地址
+        self.getTemImgs(imgFileIds);
         console.log('[数据库] [查询记录] 成功: ', res)
       },
       fail: err => {
